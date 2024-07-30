fa0c26dfe8c6cdf122dc6fd541c82814aa453ce5
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2024-07-31 01:11:24 +0700

    search solution (have not tested yet)

--- kotazk_backend/src/main/java/com/taskmanagement/kotazk/repository/IWorkSpaceRepository.java
+++ kotazk_backend/src/main/java/com/taskmanagement/kotazk/repository/IWorkSpaceRepository.java
@@ -3,8 +3,9 @@ package com.taskmanagement.kotazk.repository;
 import com.taskmanagement.kotazk.entity.User;
 import com.taskmanagement.kotazk.entity.WorkSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IWorkSpaceRepository extends JpaRepository<WorkSpace, Long> {
+public interface IWorkSpaceRepository extends JpaRepository<WorkSpace, Long>, JpaSpecificationExecutor<WorkSpace> {
 }
--- kotazk_backend/src/main/java/com/taskmanagement/kotazk/service/IWorkSpaceService.java
+++ kotazk_backend/src/main/java/com/taskmanagement/kotazk/service/IWorkSpaceService.java
@@ -1,6 +1,7 @@
 package com.taskmanagement.kotazk.service;
 
 import com.taskmanagement.kotazk.payload.request.workspace.WorkSpaceRequestDto;
+import com.taskmanagement.kotazk.payload.response.common.PageResponse;
 import com.taskmanagement.kotazk.payload.response.workspace.WorkSpaceDetailResponseDto;
 import com.taskmanagement.kotazk.payload.response.workspace.WorkSpaceSummaryResponseDto;
 
@@ -9,6 +10,10 @@ public interface IWorkSpaceService {
     WorkSpaceDetailResponseDto update(Long id, WorkSpaceRequestDto workSpace);
     Boolean delete(Long id);
     Boolean softDelete(Long id);
+    Boolean archived(Long id);
+    Boolean restore(Long id);
     WorkSpaceDetailResponseDto getDetail(Long id);
     WorkSpaceSummaryResponseDto getSummary(Long id);
+    PageResponse<WorkSpaceSummaryResponseDto> getSummaryList(Long pageNum, Long pageSize);
+    PageResponse<WorkSpaceDetailResponseDto> getDetailList(Long pageNum, Long pageSize);
 }
